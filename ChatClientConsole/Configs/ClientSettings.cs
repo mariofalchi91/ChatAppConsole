60345d723ba482b795069f7a55f0e864ff65ffff
mario <mariofalchi91@gmail.com>
2026-05-28 22:40:48 +0200

    fix test

--- ChatClientConsole/Configs/ClientSettings.cs
+++ ChatClientConsole/Configs/ClientSettings.cs
@@ -2,7 +2,7 @@
 
 namespace ChatClientConsole.Configs;
 
-public class ClientSettings
+public class ClientSettings : IValidatableObject
 {
     [Required(AllowEmptyStrings = false, ErrorMessage = "ServerUrl is required")]
     [Url(ErrorMessage = "Invalid URL")]
@@ -10,6 +10,23 @@ public class ClientSettings
 
     [Required]
     public E2EPrivateSettings E2EPrivate { get; init; } = new();
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (E2EPrivate is null)
+        {
+            yield return new ValidationResult("E2EPrivate settings are required.", [nameof(E2EPrivate)]);
+            yield break;
+        }
+
+        if (E2EPrivate.EnableLocalKeyVault && string.IsNullOrWhiteSpace(E2EPrivate.LocalKeyVaultPath))
+        {
+            yield return new ValidationResult(
+                "LocalKeyVaultPath is required when local key vault is enabled.",
+                [nameof(E2EPrivate.LocalKeyVaultPath)]
+            );
+        }
+    }
 }
 
 public class E2EPrivateSettings
